9745b4b2d50c0dd47d504f875fb2f53fb03dffe1
kimhep <kim.hepperle@gmail.com>
2022-10-27 11:27:49 +0000

    Git said: "Let there be light." And there was light.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,14 +1,24 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        Hello {props.name}, you are {props.age} years old
       </p>
     </div>
   )
 }
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+export default App
